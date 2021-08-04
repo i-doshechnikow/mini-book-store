d18d9947f85e42a0c08ddaccf607e8bdd77edef5
iliya <iliya.yurievich@yandex.ru>
2021-08-04 12:36:01 +0300

    edit readme

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -3,7 +3,7 @@ export default class BookStoreService {
     data = [{
             id: '1',
             title: 'React & Redux',
-            author: 'iliya y. doshech',
+            author: 'A. Bencs, E. Porcello',
             price: 1200,
             coverImage: 'https://www.bookvoed.ru/files/1836/45/00/93/7.jpeg',
 
@@ -11,7 +11,7 @@ export default class BookStoreService {
         {
             id: '2',
             title: 'Node & Express',
-            author: 'niki rodzh',
+            author: 'Iton Brown',
             price: 1250,
             coverImage: 'https://www.bookvoed.ru/files/1836/11/52/57/88.jpeg'
         }
